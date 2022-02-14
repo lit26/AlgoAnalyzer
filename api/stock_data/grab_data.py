4c6264a134b5618e05c388eecbc6ee5716ab70c2
Tianning Li <ltianningli@gmail.com>
2022-02-14 15:15:50 -0800

    update getting data

--- api/stock_data/grab_data.py
+++ api/stock_data/grab_data.py
@@ -2,26 +2,27 @@ import os
 import pandas as pd
 import yfinance as yf
 
-base_path = 'api/stock_data/data/'
+BASE_PATH = 'api/stock_data/data/'
+PERIOD = '5y'
 
 def get_data(ticker, timeframe):
-    data = yf.download(ticker, interval=timeframe)
+    data = yf.download(ticker, interval=timeframe, period=PERIOD)
     starttime = data.index[0].strftime("%Y-%m-%d")
     endtime = data.index[-1].strftime("%Y-%m-%d")
-    data.to_csv(f'{base_path}{ticker}_{timeframe}.csv')
+    data.to_csv(f'{BASE_PATH}{ticker}_{timeframe}.csv')
     return starttime, endtime
 
 def check_exists(ticker, timeframe):
-    if not os.path.exists(f'{base_path}{ticker}_{timeframe}.csv'):
+    if not os.path.exists(f'{BASE_PATH}{ticker}_{timeframe}.csv'):
         return get_data(ticker, timeframe)
     return None
 
 def read_data(ticker, timeframe):
-    df = pd.read_csv(f'{base_path}{ticker}_{timeframe}.csv')
+    df = pd.read_csv(f'{BASE_PATH}{ticker}_{timeframe}.csv')
     return df
 
 def delete_data(ticker, timeframe):
     try:
-        os.remove(f'{base_path}{ticker}_{timeframe}.csv')
+        os.remove(f'{BASE_PATH}{ticker}_{timeframe}.csv')
     except:
         pass
