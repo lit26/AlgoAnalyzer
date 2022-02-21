5f6ec1bc39da7e79a9fc5ecbcda1b63e81299332
Tianning Li <ltianningli@gmail.com>
2022-02-21 15:16:13 -0800

    add click and resize panels

--- frontend/src/components/Layout/index.tsx
+++ frontend/src/components/Layout/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import Topbar from '../Topbar';
+import Split from 'react-split';
+import './Layout.scss';
 
 const Layout: React.FC = () => {
     return (
         <div>
             <Topbar />
+            <Split className="Layout__cols" gutterSize={5} sizes={[75, 25]}>
+                {/* Left panel */}
+                <Split direction="vertical" gutterSize={5} sizes={[75, 25]}>
+                    <div>Chart</div>
+                    <div>Tester panel</div>
+                </Split>
+                {/* Right panel */}
+                <Split direction="vertical" gutterSize={4}>
+                    <div>Input panel</div>
+                    <div>General result panel</div>
+                </Split>
+            </Split>
         </div>
     );
 };
