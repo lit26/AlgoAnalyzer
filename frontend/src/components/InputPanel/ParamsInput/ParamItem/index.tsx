01f9e4cdf994712d0e2de626107b5fb16b6b50ba
Tianning Li <ltianningli@gmail.com>
2022-02-28 14:31:04 -0800

    update input panel

--- frontend/src/components/InputPanel/ParamsInput/ParamItem/index.tsx
+++ frontend/src/components/InputPanel/ParamsInput/ParamItem/index.tsx
@@ -1,15 +1,48 @@
 import React from 'react';
+import Checkbox from '@mui/material/Checkbox';
+import './ParamItem.scss';
+import { InputBase } from '@mui/material';
 
 interface ParamItemProps {
     name: string;
-    defaultParam: any;
+    defaultParam: number | boolean | string;
+    value: number | boolean | string;
+    onChange: (name: string, value: number | boolean | string) => void;
 }
 
-const ParamItem: React.FC<ParamItemProps> = ({ name, defaultParam }) => {
+const ParamItem: React.FC<ParamItemProps> = ({
+    name,
+    defaultParam,
+    value,
+    onChange,
+}) => {
+    const handleNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        onChange(name, event.target.checked);
+    };
+
+    const handleTextChange = (
+        event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>,
+    ) => {
+        onChange(name, event.target.value);
+    };
+
     return (
-        <div>
+        <div className="ParamItem">
             <div>{name}</div>
-            <div>{defaultParam}</div>
+            <div className="ParamItem__input">
+                {typeof value === 'boolean' && (
+                    <Checkbox checked={value} onChange={handleNumberChange} />
+                )}
+                {(typeof value === 'number' || typeof value === 'string') && (
+                    <InputBase
+                        type={
+                            typeof defaultParam === 'number' ? 'number' : 'text'
+                        }
+                        value={value}
+                        onChange={handleTextChange}
+                    />
+                )}
+            </div>
         </div>
     );
 };
--- frontend/src/components/InputPanel/ParamsInput/index.tsx
+++ frontend/src/components/InputPanel/ParamsInput/index.tsx
@@ -1,11 +1,51 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 import { useBacktest } from '../../../context/BacktestContext';
 import ParamItem from './ParamItem';
 import './ParamsInput.scss';
 
 const ParamsInput: React.FC = () => {
-    const { currentStrategy } = useBacktest();
-    console.log(currentStrategy);
+    const { currentStrategy, setCurrentStrategy } = useBacktest();
+
+    const resetParams = () => {
+        if (currentStrategy.params) {
+            setCurrentStrategy({
+                name: currentStrategy.name,
+                params: currentStrategy.params.map(param => ({
+                    ...param,
+                    current: param.default,
+                })),
+            });
+        }
+    };
+
+    const handleParamChange = (
+        name: string,
+        value: number | boolean | string,
+    ) => {
+        if (currentStrategy.params) {
+            setCurrentStrategy({
+                name: currentStrategy.name,
+                params: currentStrategy.params.map(param => {
+                    if (param.name === name) {
+                        return {
+                            ...param,
+                            current:
+                                typeof param.default === 'number' &&
+                                typeof value === 'string'
+                                    ? parseFloat(value)
+                                    : value,
+                        };
+                    } else {
+                        return param;
+                    }
+                }),
+            });
+        }
+    };
+
+    const updateParams = () => {};
+
     return (
         <>
             {/* Params input */}
@@ -16,6 +56,8 @@ const ParamsInput: React.FC = () => {
                             key={`param_${index}`}
                             name={param.name}
                             defaultParam={param.default}
+                            value={param.current}
+                            onChange={handleParamChange}
                         />
                     ))}
             </div>
@@ -23,7 +65,14 @@ const ParamsInput: React.FC = () => {
             <hr className="subDivider" />
 
             {/* Change buttons */}
-            <div className="ParamsInput__buttons">buttons</div>
+            <div className="ParamsInput__buttons">
+                <Button size="small" variant="outlined" onClick={resetParams}>
+                    Default
+                </Button>
+                <Button size="small" variant="contained" onClick={updateParams}>
+                    Ok
+                </Button>
+            </div>
         </>
     );
 };
